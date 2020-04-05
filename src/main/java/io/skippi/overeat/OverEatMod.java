7ffc32887c4391aa688b636b32162cfb9828c7a0
skippi <jbtcao@gmail.com>
2020-04-04 22:22:15 -0500

    Made every item always edible

--- src/main/java/io/skippi/overeat/OverEatMod.java
+++ src/main/java/io/skippi/overeat/OverEatMod.java
@@ -1,28 +1,42 @@
 package io.skippi.overeat;
 
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.stream.Collectors;
 
 @Mod("overeat")
 public class OverEatMod {
   private static final Logger LOGGER = LogManager.getLogger();
 
   public OverEatMod() {
+    final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    modEventBus.addListener(this::setup);
+
+    // MinecraftForge.EVENT_BUS.addListener(this::serverStarting);
     MinecraftForge.EVENT_BUS.register(this);
+  }
+
+  private void setup(final FMLCommonSetupEvent event) {
+    LOGGER.info("OverEat::setup");
+
+    for (Item item : ForgeRegistries.ITEMS) {
+      if (!item.isFood()) {
+        continue;
+      }
+
+      final Food food = item.getFood();
+      final String canAlwaysEatFieldName = "field_221473_d";
+      ObfuscationReflectionHelper.setPrivateValue(Food.class, food, true, canAlwaysEatFieldName);
+
+      LOGGER.info(String.format("%s: %s", item.getRegistryName(), food.canEatWhenFull()));
+    }
   }
 }
