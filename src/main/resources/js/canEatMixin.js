6b6eb0820ab502854f5f82afb8472b8205bd7181
skippi <jbtcao@gmail.com>
2020-04-24 18:12:22 -0500

    Fixed coremod not applying on real clients.
    - Real clients have an obfuscated code layout. Using the obfuscated
      method name does what we want.
    - The reason we use `ASMAPI.mapMethod` is because using the plain
      SRG name makes the mod untestable in the dev environment, despite
      working on real clients.

--- src/main/resources/js/canEatMixin.js
+++ src/main/resources/js/canEatMixin.js
@@ -3,12 +3,16 @@ var InsnNode = Java.type("org.objectweb.asm.tree.InsnNode")
 var VarInsnNode = Java.type("org.objectweb.asm.tree.VarInsnNode")
 var Opcodes = Java.type("org.objectweb.asm.Opcodes")
 
+var asmApi = Java.type('net.minecraftforge.coremod.api.ASMAPI');
 
 var ALOAD = function (argNo) { return new VarInsnNode(Opcodes.ALOAD, argNo) }
 var ICONST_1 = new InsnNode(Opcodes.ICONST_1)
 var IRETURN = new InsnNode(Opcodes.IRETURN)
-
-
+var INVOKESTATIC_canEatWithConfig = asmApi.buildMethodCall(
+    "io/skippi/overeat/OverEatMod",
+    "canEatWithConfig",
+    "(Lnet/minecraft/entity/player/PlayerEntity;)Z",
+    asmApi.MethodType.STATIC)
 
 function initializeCoreMod() {
     return {
@@ -16,21 +20,13 @@ function initializeCoreMod() {
             'target': {
                 'type': 'METHOD',
                 'class': 'net.minecraft.entity.player.PlayerEntity',
-                'methodName': 'canEat',
+                'methodName': asmApi.mapMethod('func_71043_e'), // PlayerEntity#canEat
                 'methodDesc': '(Z)Z'
             },
             'transformer': function(method) {
-                var asmApi = Java.type('net.minecraftforge.coremod.api.ASMAPI');
-
-                var canEatCall = asmApi.buildMethodCall(
-                    "io/skippi/overeat/OverEatMod",
-                    "canEatWithConfig",
-                    "(Lnet/minecraft/entity/player/PlayerEntity;)Z",
-                    asmApi.MethodType.STATIC)
-
                 var mixin = new InsnList()
                 mixin.add(ALOAD(0))
-                mixin.add(canEatCall)
+                mixin.add(INVOKESTATIC_canEatWithConfig)
                 mixin.add(IRETURN)
 
                 method.instructions.insert(mixin)
