bca92002e4aa97755e39b0827ad7871d325dccee
skippi <jbtcao@gmail.com>
2020-04-24 18:14:44 -0500

    Renamed coremod variable to match class name
    - More indicative of original java code.

--- src/main/resources/js/canEatMixin.js
+++ src/main/resources/js/canEatMixin.js
@@ -3,16 +3,16 @@ var InsnNode = Java.type("org.objectweb.asm.tree.InsnNode")
 var VarInsnNode = Java.type("org.objectweb.asm.tree.VarInsnNode")
 var Opcodes = Java.type("org.objectweb.asm.Opcodes")
 
-var asmApi = Java.type('net.minecraftforge.coremod.api.ASMAPI');
+var ASMAPI = Java.type('net.minecraftforge.coremod.api.ASMAPI');
 
 var ALOAD = function (argNo) { return new VarInsnNode(Opcodes.ALOAD, argNo) }
 var ICONST_1 = new InsnNode(Opcodes.ICONST_1)
 var IRETURN = new InsnNode(Opcodes.IRETURN)
-var INVOKESTATIC_canEatWithConfig = asmApi.buildMethodCall(
+var INVOKESTATIC_canEatWithConfig = ASMAPI.buildMethodCall(
     "io/skippi/overeat/OverEatMod",
     "canEatWithConfig",
     "(Lnet/minecraft/entity/player/PlayerEntity;)Z",
-    asmApi.MethodType.STATIC)
+    ASMAPI.MethodType.STATIC)
 
 function initializeCoreMod() {
     return {
@@ -20,7 +20,7 @@ function initializeCoreMod() {
             'target': {
                 'type': 'METHOD',
                 'class': 'net.minecraft.entity.player.PlayerEntity',
-                'methodName': asmApi.mapMethod('func_71043_e'), // PlayerEntity#canEat
+                'methodName': ASMAPI.mapMethod('func_71043_e'), // PlayerEntity#canEat
                 'methodDesc': '(Z)Z'
             },
             'transformer': function(method) {
